9b58ea2c92953f4eb3b880a17bf1eecee7b90614
Ankit Verma <ankitv78389@gmail.com>
2025-01-14 23:54:57 +0530

    :hammer:[refractor][added 24th challenge.]

---
+++ challenge 24 - Popularity Contest/index.js
@@ -0,0 +1,27 @@
+import postData from "./data.js";
+
+/* Popularity Contest 
+
+Iggy the Influencer and Toby the Tiktoker are dying to know
+who's more popular on social media. 
+
+Toby's TikToks get an average of 400 likes. On average, how many
+likes do Iggy's Instagram posts get? 
+
+In data.js you'll find a list of Iggy's recent posts. 
+Use reduce() to write a function that returns the average number of likes.
+To find the average, add up the total number of likes, then divide
+by the total number of posts.
+*/
+
+function calcAverageLikes(data){
+  const averageLikesIggy= data.reduce((acc,curr)=>{
+     acc+=curr.likes
+     return (acc)
+  },0)
+  
+  return Math.round(averageLikesIggy/data.length)
+} 
+
+
+console.log(calcAverageLikes(postData))
